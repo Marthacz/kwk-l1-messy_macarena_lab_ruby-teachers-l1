f3ff26d0c1b212fd0fd7cc79213854b872abc3c9
Martha Czernuszenko <mvczer@gmail.com>
2018-08-05 00:47:51 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -10,8 +10,14 @@ puts "Step 1: Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
 puts "Step 2: Left arm out in front of you, palm facing down."
-
-# Step 3: FIXME
+p
+3. "Turn your right palm up."
+4. "Turn your left palm up."
+5. "Place your right hand on your left shoulder."
+6. "Place your left hand on your right shoulder."
+7. "Bring your right hand up and around to the back of your head."
+8. "Bring your left hand up and around to the back your head."
+```XME
 puts "Step 3: Turn your right palm up."
 
 # Step 4: FIXME
